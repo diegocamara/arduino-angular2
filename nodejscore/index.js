2926d01e958b689eab05ac5b2668a9feaffed02c
Diego <diegoluizjava.java@gmail.com>
2017-04-24 17:42:57 -0300

    ajuste no index.js do nodejscore

--- nodejscore/index.js
+++ nodejscore/index.js
@@ -2,9 +2,10 @@ var cfg = require('_/config');
 var log = require('_/log');
 var models = require('_/app/models');
 var app = require('_/app');
-var robotjs = require();
-
-
+var five = require('johnny-five');
+var Robot = require('./robotjs/robot/robot');
+var board = new five.Board({ repl: false });
+var mqtt = require('mqtt');
 /*
 models.mongoose.connection.on('error', console.error.bind(console, 'Connection error:'));
 
@@ -17,6 +18,39 @@ models.mongoose.connection.once('open', function(callback){
 
 */
 
-app.listen(cfg.port, function(){
-        console.log('app listening on port', cfg.port);
+var robot = new Robot({
+    motionOptions: {
+        baseServoOptions: {
+            pin: 0,
+            startAt: 90
+        },
+        headServoOptions: {
+            pin: 1,
+            startAt: 80
+        }
+    }
+}, board);
+
+app.listen(cfg.port, function () {
+
+    console.log('App listening on port', cfg.port);
+
+    var brokerUrl = 'mqtt://localhost';
+        
+    var client = mqtt.connect(brokerUrl);
+
+    client.on('connect', function(){
+        console.log('System connect with ' + brokerUrl);
+        client.subscribe('#').on('message', function(topic, message){
+            console.log(message.toString());
+        });
+
+    });
+
+    console.log('System looking for compatible hardware.');
+    board.on('ready', function () {
+        console.log('Hardware ready!.');
+        robot.initSystem();
+    });
+
 });
